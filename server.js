424c1b913a1248f994609dc17bf9dfd38b16bd51
Aurelien Dohant <dohant.aurelien@gmail.com>
2015-10-05 11:38:25 +0200

    test

---
+++ server.js
@@ -0,0 +1,50 @@
+var Hapi = require('hapi');
+var Good = require('good');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route([
+  {
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+  },
+  {
+    method: 'GET',
+    path: '/test1',
+    handler: function (request, reply) {
+        reply('test 1');
+    }
+  },
+  {
+    method: 'GET',
+    path: '/test2',
+    handler: function (request, reply) {
+        reply('test 2');
+    }
+  }
+]);
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
